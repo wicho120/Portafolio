dbbde9e02d7a5f859ef71f370f97c84fd68eebc8
Wicho120 <35779309+wicho120@users.noreply.github.com>
2024-09-03 03:37:15 -0500

    feat: :sparkles: actualizacion de github Page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({   
+
   plugins: [react()],
-})
+  base: '/mi-proyecto/',   
+ // Ajusta esto según la ubicación de tu proyecto en GitHub Pages
+});
